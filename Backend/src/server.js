2333265450ffec4b491247777e2b7d59774313f8
Dulneth Santhuka <dulneth.sa@gmail.com>
2025-10-14 18:09:40 +0530

    Initial commit

---
+++ Backend/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import "dotenv/config";
+import { connectDB } from "./lib/db.js";
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
