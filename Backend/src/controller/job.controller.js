c11bfe65d119331b58f06ad0010542d6a58e2f42
Dulneth Santhuka <dulneth.sa@gmail.com>
2025-10-17 01:52:33 +0530

    Create job controller and configure stripe

---
+++ Backend/src/controller/job.controller.js
@@ -0,0 +1,13 @@
+import JobPost from "../model/JobPost.js";
+import Organization from "../model/Organization.js";
+import Stripe from "../../config/stripe.js";
+
+const jobController = {
+  createJobAndPay: async (req, res) => {},
+  listJobs: async (req, res) => {},
+  getSingleJob: async (req, res) => {},
+  updateJob: async (req, res) => {},
+  getRecommendedJobs: async (req, res) => {},
+};
+
+export default jobController;
