3aa857b83eb34ab0fb302c9554cc9fd1aa2865e8
Dulneth Santhuka <dulneth.sa@gmail.com>
2025-10-22 19:29:49 +0530

    Login and signup pages

---
+++ Mobile/app/(auth)/signup.jsx
@@ -0,0 +1,265 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Dimensions,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
+
+import { Picker } from "@react-native-picker/picker";
+
+export default function signup() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [role, setRole] = useState("");
+  const [selectedValue, setSelectedValue] = useState("java");
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View style={styles.container}>
+        <View style={styles.card}>
+          {/* HEADER */}
+          <View style={styles.header}>
+            <Text style={styles.title}>Job Nest</Text>
+            <Text style={styles.subtitle}>Share your favorite reads</Text>
+          </View>
+          <View style={styles.formContainer}>
+            {/* EMAIL INPUT */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Email</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="mail-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="johndoe@gmail.com"
+                  value={email}
+                  placeholderTextColor={COLORS.placeholderText}
+                  onChangeText={setEmail}
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                />
+              </View>
+            </View>
+            {/* ROLE INPUT */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Select Role</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="users"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <Picker
+                  selectedValue={selectedValue}
+                  onValueChange={(itemValue) => setSelectedValue(itemValue)}
+                  style={styles.input}
+                  placeholder="johndoe@gmail.com"
+                >
+                  <Picker.Item label="Select a role" value="" enabled={false} />
+                  <Picker.Item
+                    label="Java"
+                    value="java"
+                    style={styles.pickerItem}
+                  />
+                  <Picker.Item
+                    label="JavaScript"
+                    value="js"
+                    style={styles.pickerItem}
+                  />
+                  <Picker.Item
+                    label="Python"
+                    value="python"
+                    style={styles.pickerItem}
+                  />
+                </Picker>
+                {/* <TextInput
+                  style={styles.input}
+                  placeholder="johndoe@gmail.com"
+                  value={email}
+                  placeholderTextColor={COLORS.placeholderText}
+                  onChangeText={setEmail}
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                /> */}
+              </View>
+            </View>
+
+            {/* PASSWORD INPUT */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Password</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="lock-closed-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="******"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={!showPassword}
+                />
+                <TouchableOpacity
+                  onPress={() => setShowPassword(!showPassword)}
+                  style={styles.eyeIcon}
+                >
+                  <Ionicons
+                    name={showPassword ? "eye-outline" : "eye-off-outline"}
+                    size={20}
+                    color={COLORS.primary}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+            {/* CONFIRM PASSWORD INPUT */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Confirm Password</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="lock-closed-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="******"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={!showPassword}
+                />
+                <TouchableOpacity
+                  onPress={() => setShowPassword(!showPassword)}
+                  style={styles.eyeIcon}
+                >
+                  <Ionicons
+                    name={showPassword ? "eye-outline" : "eye-off-outline"}
+                    size={20}
+                    color={COLORS.primary}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+import COLORS from "../../constants/colors";
+const { width } = Dimensions.get("window");
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    backgroundColor: COLORS.background,
+    padding: 20,
+    justifyContent: "center",
+  },
+  card: {
+    backgroundColor: COLORS.cardBackground,
+    borderRadius: 16,
+    padding: 24,
+    shadowColor: COLORS.black,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 4,
+    borderWidth: 2,
+    borderColor: COLORS.border,
+  },
+  header: {
+    alignItems: "center",
+    marginBottom: 32,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "700",
+    fontFamily: "JetBrainsMono-Medium",
+    color: COLORS.primary,
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: COLORS.textSecondary,
+    textAlign: "center",
+  },
+  formContainer: { marginBottom: 16 },
+  inputGroup: { marginBottom: 20 },
+  label: {
+    fontSize: 14,
+    marginBottom: 8,
+    color: COLORS.textPrimary,
+    fontWeight: "500",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: COLORS.inputBackground,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: COLORS.border,
+    paddingHorizontal: 12,
+  },
+  inputIcon: { marginRight: 10 },
+  input: {
+    flex: 1,
+    height: 48,
+    color: COLORS.textDark,
+  },
+  eyeIcon: { padding: 8 },
+  button: {
+    backgroundColor: COLORS.primary,
+    borderRadius: 12,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 16,
+    shadowColor: COLORS.black,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  buttonText: {
+    color: COLORS.white,
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 24,
+  },
+  footerText: {
+    color: COLORS.textSecondary,
+    marginRight: 5,
+  },
+  link: {
+    color: COLORS.primary,
+    fontWeight: "600",
+  },
+  pickerItem: {
+    fontSize: 14,
+  },
+});
